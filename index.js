a43235a0415a24107a1d6b00459dd9d1be488c78
Chris Parker <mrcsparker@gmail.com>
2014-12-01 22:44:56 -0600

    Added index

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  name: 'Ember CLI rasterizehtml',
+
+  treeFor: function(name) {
+    if (name !== 'vendor') { return; }
+
+    return this.treeGenerator(path.join(__dirname, 'node_modules'));
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    var options = this.app.options.rasterizehtmlOptions || {enabled: true};
+
+    if (options.enabled) {
+      this.app.import('vendor/rasterizehtml/dist/rasterizeHTML.allinone.js', {
+        exports: {
+          'rasterizehtml': [
+            'url',
+            'xmlserializer',
+            'ayepromise',
+            'inlineresources'
+          ]
+        }
+      });
+    }
+  }
+};
